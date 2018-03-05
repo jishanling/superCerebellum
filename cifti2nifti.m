0aeb579275b8b44636f8a9f2a5018948d2376279
Carlos <cherna3@uwo.ca>
2018-03-05 16:25:07 -0500

    change in the name of the output

--- cifti2nifti.m
+++ cifti2nifti.m
@@ -8,21 +8,16 @@ function cifti2nifti(image)
 % Output: NIFTI image in MNI space(2mm)
 %_________________________________________________________________________
 % Carlos Hernandez 2018
-% Amended by Maedbh King (22/02/18). Implemented formula v=inv(A)*w to go from world to voxel
-% coordinates. Also - now codes labels and not brainstructure
+% Amended by Maedbh King (22/02/18). 
 
 [dir,n]=spm_fileparts(image);
 name=strsplit(n,'.');
 
 % Read CIFTI
 cii=ft_read_cifti(image);
-
 numVox=size(cii.indexmax,1); 
 
 % Conversion from mm to vox
-%vox=(cii.pos/2)+repmat([47 63 36],size(cii.pos,1),1);
-
-% this should give the same answer as above solution ? 
 for v=1:numVox,
     tmp=inv(cii.transform)*[cii.pos(v,:),1]'; 
     vox(v,:)=tmp'; 
@@ -31,11 +26,11 @@ end
 % Write Labels
 DATA=zeros(cii.dim);
 for i=1:numVox,
-    DATA(vox(i,1),vox(i,2),vox(i,3))=cii.indexmax(i); % MK changed this to always code labels (not brainstructure)
+    DATA(vox(i,1),vox(i,2),vox(i,3))=cii.indexmax(i); 
 end
 
 % MNI 2mm data structure
-V = struct('fname',[dir 'N2C_' name{1} '.nii'],... % this should be C2N ?
+V = struct('fname',[dir 'C2N_' name{1} '.nii'],... 
             'dim',cii.dim,...
             'dt',[4 0],...
             'mat',[-2 0 0 92;0 2 0 -128;0 0 2 -74;0 0 0 1],...
