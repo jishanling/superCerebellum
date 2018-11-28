8a2a2d76d2cea7df3dbe2e22cc80ab4f05675d2e
Maedbh King <mking86@uwo.ca>
2018-11-28 09:00:16 -0800

    Add files via upload

--- sc1_sc2_ICB.m
+++ sc1_sc2_ICB.m
@@ -16,8 +16,8 @@ suitDir         ='/suit';
 caretDir        ='/surfaceCaret';
 regDir          ='/RegionOfInterest/';
 encodeDir       ='/encoding';
-contrastDir     ='/contrasts'; 
-anatDir         ='/anatomical'; 
+contrastDir     ='/contrasts';
+anatDir         ='/anatomical';
 
 funcRunNum = [51,66];  % first and last behavioural run numbers (16 runs per subject)
 
@@ -50,6 +50,7 @@ loc_AC = {[-81,-127,-144],... %s01
 
 hem={'lh','rh'};
 hemName={'LeftHem','RightHem'};
+
 
 switch what
     
@@ -92,17 +93,17 @@ switch what
         end
         
     case 'SUIT:run_all'
-        sn=varargin{1}; 
+        sn=varargin{1};
         
         sc1_sc2_ICB('SUIT:isolate_segment',sn)
         sc1_sc2_ICB('SUIT:make_maskImage',sn)
         sc1_sc2_ICB('SUIT:corr_cereb_cortex_mask',sn)
         sc1_sc2_ICB('SUIT:normalise_dartel',sn,'grey')
         sc1_sc2_ICB('SUIT:make_mask',sn,'grey')
-%         sc1_sc2_ICB('SUIT:reslice',sn)
+        %         sc1_sc2_ICB('SUIT:reslice',sn)
     case 'SUIT:isolate_segment'              % STEP 9.2:Segment cerebellum into grey and white matter
         sn=varargin{1};
-%         spm fmri
+        %         spm fmri
         for s=sn,
             suitSubjDir = fullfile(IBCDir,suitDir,'anatomicals',IBC_subjs{s});dircheck(suitSubjDir);
             source=fullfile(IBCDir,anatDir,IBC_subjs{s},'anatomical.nii');
@@ -110,17 +111,17 @@ switch what
             copyfile(source,dest);
             cd(fullfile(suitSubjDir));
             suit_isolate_seg({fullfile(suitSubjDir,'anatomical.nii')},'keeptempfiles',1);
-        end 
+        end
     case 'SUIT:make_maskImage'               % STEP 3.7:Make mask images (noskull and grey_only)
         % Make maskImage meanepi
         % example: sc1_sc2_imana('FUNC:make_maskImage',1)
         sn=varargin{1}; % subjNum
-
+        
         for s=1:length(sn),
             
             % get example func image and mask
             funcImage=dir(fullfile(IBCDir,'contrasts',IBC_subjs{sn(s)})); % get example func image and mask.
-
+            
             nam{1}  = fullfile(IBCDir,'contrasts',IBC_subjs{sn(s)}, funcImage(20).name); % contrast image
             spm_imcalc(nam, fullfile(IBCDir,'contrasts',IBC_subjs{sn(s)},'mask_gray.nii'), 'i1~=0')
         end
@@ -129,12 +130,12 @@ switch what
         % STUDY 1
         
         subjs=length(sn);
-        suitAnatDir=fullfile(IBCDir,'suit','anatomicals'); 
+        suitAnatDir=fullfile(IBCDir,'suit','anatomicals');
         
         for s=1:subjs,
             
-            cortexGrey= fullfile(suitAnatDir,IBC_subjs{sn(s)},'c3anatomical.nii'); 
-            cerebGrey = fullfile(suitAnatDir,IBC_subjs{sn(s)},'c1anatomical.nii'); 
+            cortexGrey= fullfile(suitAnatDir,IBC_subjs{sn(s)},'c3anatomical.nii');
+            cerebGrey = fullfile(suitAnatDir,IBC_subjs{sn(s)},'c1anatomical.nii');
             bufferVox = fullfile(suitAnatDir,IBC_subjs{sn(s)},'buffer_voxels.nii');
             
             % isolate overlapping voxels
@@ -163,7 +164,7 @@ switch what
         % example: sc1_sc2_imana('SUIT:normalise_dartel',1,'grey')
         sn=varargin{1}; %subjNum
         type=varargin{2}; % 'grey' or 'whole' cerebellar mask
-
+        
         for s=1:length(sn),
             cd(fullfile(IBCDir,'suit','anatomicals',IBC_subjs{sn(s)}));
             job.subjND.gray      = {'c_anatomical_seg1.nii'};
@@ -218,34 +219,51 @@ switch what
             cd(fullfile(IBCDir,'contrasts',IBC_subjs{sn(s)}))
             job.subj.resample = {source.name};
             dircheck(fullfile(IBCDir,'suit','contrasts',IBC_subjs{sn(s)}));
-%             cd(fullfile(IBCDir,'suit','contrasts',IBC_subjs{sn(s)}));
+            %             cd(fullfile(IBCDir,'suit','contrasts',IBC_subjs{sn(s)}));
             suit_reslice_dartel(job);
             temp=dir('*wd*');
             for f=1:length(temp),
                 movefile(fullfile(IBCDir,'contrasts',IBC_subjs{sn(s)},temp(f).name),fullfile(IBCDir,'suit','contrasts',IBC_subjs{sn(s)},temp(f).name));
-            end 
-
+            end
+            
             fprintf('contrasts have been resliced into suit space for %s \n\n',IBC_subjs{sn(s)})
         end
         
     case 'PREP:ICB_info'
-        sn=varargin{1}; 
+        sn=varargin{1};
         
+        % get all possible task conditions
+        idx=1;
         for s=1:length(sn),
-            conName=dir(fullfile(IBCDir,'contrasts',IBC_subjs{sn(s)},'*sess*'));  
-            T=[];
+            conName=dir(fullfile(IBCDir,'contrasts',IBC_subjs{sn(s)},'*sess*'));
             for c=1:length(conName),
-                T.condNames{c,1}=conName(c).name(8:end-4);
-                tmp=char(regexp(conName(c).name(1:6),'[0-9]','match'))';
-                T.sessNum(c,1)=tmp(2);
-                T.SN(c,1)=sn(s);
+                condName{idx,1}=conName(c).name(8:end-4);
+                idx=idx+1;
             end
         end
-        varargout={T};
+        D.condNames=unique(condName);
+        D.condNum=[1:length(D.condNames)]';
         
+        % organise across subjs
+        S=[];
+        for s=1:length(sn),
+            conName=dir(fullfile(IBCDir,'contrasts',IBC_subjs{sn(s)},'*sess*'));
+            for c=1:length(conName),
+                condName=conName(c).name(8:end-4);
+                T.condNum(c,1)=D.condNum(strcmp(D.condNames,condName));
+                T.condNames{c,1}=condName;
+                tmp=str2double(conName(c).name(5:6));
+                T.sessNum(c,1)=tmp;
+            end
+            T.SN=repmat(sn(s),length(T.condNames),1);
+            S=addstruct(S,T);
+            clear T conName tmp
+        end
+        
+        varargout={S};
     case 'PREP:avrgMask_cereb'               % STEP 11.3:
         sn=varargin{1};
-        step=varargin{2}; % 'reslice' or 'mask' 
+        step=varargin{2}; % 'reslice' or 'mask'
         % don't include s01 and s04 in the 'mask' step
         
         subjs=length(sn);
@@ -275,51 +293,141 @@ switch what
     case 'PREP:cereb:voxels'                 % STEP 11.6: Get UW cerebellar data (voxels)
         sn=varargin{1};
         
-        T=[];
+        P=24076; % # of cerebellar voxels
+        Q=117; % # of (unique) task contrasts
         
         % Load over all grey matter mask
         V=spm_vol(fullfile(IBCDir,'suit','anatomicals','cerebellarGreySUIT.nii'));
+        
+        % load PREP info
+        T=sc1_sc2_ICB('PREP:ICB_info',IBC_goodSubjs);
         
         X=spm_read_vols(V);
         grey_threshold = 0.1; % grey matter threshold
         volIndx=find(X>grey_threshold);
         [i,j,k]= ind2sub(size(X),volIndx');
         
+        S=[];
         for s=1:length(sn),
             
-            Y=sc1_sc2_ICB('PREP:ICB_info',sn(s));
+            Y=getrow(T,T.SN==sn(s));
             
             % load in normalised contrasts
             % univariately pre-whiten cerebellar voxels
             nam={};
-            contrasts=dir(fullfile(IBCDir,'suit','contrasts',IBC_subjs{sn(s)},'*wd*'));
-            for c=1:length(contrasts),
-                nam{1}=fullfile(IBCDir,'suit','contrasts',IBC_subjs{sn(s)},contrasts(c).name);
-                Vi=spm_vol(nam{1});
-                C1(c,:)=spm_sample_vol(Vi,i,j,k,0);
+            idx=1;
+            for c=1:Q,
+                condIdx=Y.condNum==c;
+                numCon=numel(condIdx(condIdx~=0));
+                for n=1:numCon,
+                    sessIdx=Y.sessNum(condIdx);
+                    CI=Y.condNames(condIdx);
+                    nam{1}=fullfile(IBCDir,'suit','contrasts',IBC_subjs{sn(s)},sprintf('wdsess%2.2d-%s.nii',sessIdx(n),CI{n}));
+                    Vi=spm_vol(nam{1});
+                    C1(idx,:)=spm_sample_vol(Vi,i,j,k,0);
+                    idx=idx+1;
+                end
             end
+            
+            % make zero values nan
+            C1(C1==0)=nan;
             
             % write out new structure ('Y_info')
             Y.data=C1;
             Y.nonZeroInd=repmat(volIndx',size(C1,1),1);
             
-            outName= fullfile(IBCDir,'suit','contrasts','cereb_avrgDataStruct.mat');
+            outName=fullfile(IBCDir,'suit','contrasts','cereb_avrgDataStruct.mat');
             fprintf('cerebellar voxels computed for %s \n',IBC_subjs{sn(s)});
             clear C1
             
-            T=addstruct(T,Y);
+            S=addstruct(S,Y);
             clear Y
         end
         volIndx=volIndx';
-        save(outName,'T','volIndx','V');
+        save(outName,'S','volIndx','V');
         
-    case 'EVAL:ICB'% Evaluate group Map
+    case 'ACTIVITY:map2surf'
+        sn=varargin{1}; % 'group' or <subjNum>
+        
+        % group or individual ?
+        if ~strcmp(sn,'group'),
+            outDir=fullfile(studyDir{2},caretDir,sprintf('x%s',IBC_subjs{sn}),'cerebellum'); dircheck(outDir)
+        else
+            outDir=fullfile(studyDir{2},caretDir,'suit_flat','glm4');
+        end
+        
+        load(fullfile(IBCDir,'suit','contrasts','cereb_avrgDataStruct.mat'));
+        
+        SN=unique(S.SN);
+        CN=unique(S.condNum);
+        
+        % set up volume info
+        Yy=zeros(length(CN),length(SN),V.dim(1)*V.dim(2)*V.dim(3));
+        C{1}.dim=V.dim;
+        C{1}.mat=V.mat;
+        
+        % loop over subjs
+        for s=1:length(SN),
+            for c=1:length(CN),
+                condNames{c}=char(unique(S.condNames(S.condNum==CN(c))));
+                idx=S.SN==SN(s) & S.condNum==CN(c);
+                B(c,s,:)=nanmean(S.data(idx,:),1);
+            end
+        end
+        
+        % subtract baseline - do we have a baseline here ??
+        %         baseline=nanmean(B,1);
+        %         B=bsxfun(@minus,B,baseline);
+        
+        % z score the activity patterns
+        B=zscore(B);
+        
+        Yy(:,:,volIndx)=B;
+        Yy=permute(Yy,[2 1 3]);
+        
+        indices=nanmean(Yy,1);
+        indices=reshape(indices,[size(indices,2),size(indices,3)]);
+        
+        % map vol2surf
+        indices=reshape(indices,[size(indices,1) V.dim(1),V.dim(2),V.dim(3)]);
+        for i=1:size(indices,1),
+            data=reshape(indices(i,:,:,:),[C{1}.dim]);
+            C{i}.dat=data;
+        end
+        P=caret_suit_map2surf(C,'space','SUIT','stats','nanmean','column_names',condNames);  % MK created caret_suit_map2surf to allow for output to be used as input to caret_save
+        
+        % save out metric
+        if strcmp(sn,'group'),
+            outName='IBC_contrasts';
+        else
+            outName=sprintf('%s_IBC_contrasts',IBC_subjs{sn});
+        end
+        caret_save(fullfile(outDir,sprintf('%s.metric',outName)),P);
+    case 'ACTIVITY:vol2surf'
+        % this function takes any labelled volume (already in SUIT space)
+        % and plots to the surface
+        sn=varargin{1};
+        inputMap=varargin{2}; % some options are 'Buckner_7Networks','SC1_9cluster','lob10', 'Cole_10Networks', 'SC2_90cluster' etc
+        
+        mapDir=fullfile(IBCDir,'suit','contrasts',IBC_subjs{sn},sprintf('wdsess%s.nii',inputMap));
+        
+        Vo=spm_vol(fullfile(mapDir));
+        Vi=spm_read_vols(Vo);
+        Vv{1}.dat=Vi;
+        Vv{1}.dim=Vo.dim;
+        Vv{1}.mat=Vo.mat;
+        
+        M=caret_suit_map2surf(Vv,'space','SUIT');
+        
+        suit_plotflatmap(M.data)
+    case 'EVAL:ICB'% Evaluate group Map on IBC data
         sn=varargin{1}; % 'group' or <subjNum>
         mapType=varargin{2}; % options are 'lob10','lob26','Buckner_17Networks','Buckner_7Networks', 'Cole_10Networks','SC<studyNum>_<num>cluster'
-
+        
         % load in func data to test (e.g. if map is sc1; func data should
         % be sc2)
-        load(fullfile(IBCDir,'suit','contrasts','cereb_avrgDataStruct.mat')); 
+        load(fullfile(IBCDir,'suit','contrasts','cereb_avrgDataStruct.mat'));
+        T=S;
         
         % evaluating the group or the individual ?
         if strcmp(sn,'group'),
@@ -354,7 +462,55 @@ switch what
             R.crossval = zeros(length(R.corr),1);
             RR = addstruct(RR,R);
         end;
-        save(outName,'-struct','RR');     
+        save(outName,'-struct','RR');
+    case 'EVAL:PLOT:CURVES'
+        mapType=varargin{1}; % options are 'lob10','lob26','bucknerRest','SC<studyNum>_<num>cluster', or 'SC<studyNum>_POV<num>'
+        type=varargin{2}; % 'group' or 'indiv'
+        
+        vararginoptions({varargin{3:end}},{'CAT','sn'}); % option if doing individual map analysis
+        
+        switch type,
+            case 'group'
+                T=load(fullfile(studyDir{2},encodeDir,'glm4',sprintf('groupEval_%s',mapType),'spatialBoundfunc_ICB.mat'));
+            case 'indiv'
+                T=[];
+                for s=1:length(sn)
+                    tmp=load(fullfile(studyDir{2},'encoding','glm4',MDTB_subjs{sn(s)},sprintf('%s_spatialBoundfunc_ICB.mat',mapType)));
+                    S=getrow(tmp,tmp.SN==sn(s)); % only predicting the same subject !
+                    T=addstruct(T,S);
+                end
+            otherwise
+                fprintf('no such case')
+        end
+        
+        if exist('CAT'),
+            xyplot(T.dist,T.corr,T.dist,'split',T.bwParcel,'subset',T.crossval==0 & T.dist<=35,'CAT',CAT,'leg',{'within','between'},'leglocation','SouthEast');
+        else
+            xyplot(T.dist,T.corr,T.dist,'split',T.bwParcel,'subset',T.crossval==0 & T.dist<=35,'leg',{'within','between'},'leglocation','SouthEast');
+        end
+        
+    case 'AXES:group_curves' % make separate graphs for 'lob10','Buckner_7Networks','Buckner_17Networks','Cole_10Networks','SC12_10cluster'
+        toPlot=varargin{1}; % 'SC12_10cluster'
+        
+        % Aesthetics
+        CAT.markertype='none';
+        CAT.errorwidth=.5;
+        CAT.linecolor={'r','k'};
+        CAT.errorcolor={'r','k'};
+        CAT.linewidth={2, 2};
+        CAT.linestyle={'-','-'};
+        
+        sc1_sc2_ICB('EVAL:PLOT:CURVES',toPlot,'group','CAT',CAT);
+        
+        % Labelling
+        set(gca,'YLim',[0 0.3],'XLim',[0 35],'FontSize',10,'xtick',[0:5:35],'XTickLabel',{'0','','','','','','','35'}); %
+        xlabel('Spatial Distances (mm)');
+        ylabel('Activity Correlation (R)');
+        %         title(plotName);
+        set(gcf,'units','centimeters','position',[5,5,15,15])
+        %         axis('auto')
+        % do stats
+        %         sc1_sc2_ICB('EVAL:STATS:CURVES',toPlot)
 end
 
 
